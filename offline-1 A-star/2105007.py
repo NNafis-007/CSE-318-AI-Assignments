6731fd6f8c1c2f358d6a576d7d2a3ad54fdc7477
Nafis Nahian <nafisnahian71@gmail.com>
2025-04-20 00:59:32 +0600

    fixed pushing in closed list

--- offline-1 A-star/2105007.py
+++ offline-1 A-star/2105007.py
@@ -2,7 +2,7 @@ from N_Puzzle import N_Puzzle
 from heuristics import hamming_distance, manhattan_distance, euclidean_distance, linear_conflict
 from PQueue import PriorityQueue
 
-h_n = manhattan_distance
+h_n = hamming_distance
 
 #Take input
 k = int(input("enter matrix size then the elements\n"))
@@ -25,14 +25,23 @@ else:
     pqueue = PriorityQueue()
     pqueue.push(0, initial_state)
 
-    # open_list : list[N_Puzzle] = []
-    explored = 1
-    closed_list : list[N_Puzzle] = []
+    heuristics = [hamming_distance, manhattan_distance, euclidean_distance, linear_conflict]
+    h_n = heuristics[1]
 
+    # open_list : list[N_Puzzle] = []
+    #closed_list : list[N_Puzzle] = []
+    closed_set: set = set()  # To store string representations of grids
+    i = 0
+    explored = 0
+    expanded = 0
     while pqueue.size() != 0:
+        # i += 1
+        # print(f"iteration number : {i}")
         _, curr_state = pqueue.pop()
-        closed_list.append(curr_state)
-        print(curr_state)
+        #closed_list.append(curr_state)
+        closed_set.add(str(curr_state.grid))
+        expanded += 1
+        #print(curr_state)
         if(curr_state.is_correct_config()):
             break
         
@@ -42,7 +51,7 @@ else:
             nei_puzzle.moves_count = curr_state.moves_count + 1
             nei_puzzle.priority = nei_puzzle.moves_count + h_n(nei_puzzle.grid)
             nei_puzzle.parent = curr_state
-            if nei_puzzle not in closed_list:
+            if str(nei_puzzle.grid) not in closed_set:
                 pqueue.push(nei_puzzle.priority, nei_puzzle)
                 explored += 1
 
@@ -57,5 +66,8 @@ else:
     states_path.append(curr_state)
     states_path = states_path[::-1]
 
+    for state in states_path:
+        print(state)
+
     print("open list : ", explored)
-    print("closed list : ", len(closed_list))
+    print("closed list : ", expanded)
